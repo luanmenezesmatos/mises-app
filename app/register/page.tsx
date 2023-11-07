2b31e2459fea0c4a19d2ce32ffedb57a02bd10ff
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-11-07 15:17:30 -0300

    delete: pasta do app/register para app/autenticacao/cadastrar

--- app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreateAccountForm from "@/components/auth/create-account-form";
-
-export default async function Register() {
-  return (
-    <div className="flex flex-col h-screen w-full justify-center items-center">
-      <CreateAccountForm />
-    </div>
-  )
-}
